a8039e66543b042b03d0e0136dfa3440317100a7
Sambhasher <sambhavj9@gmail.com>
2021-12-13 15:49:51 +0530

    FGUI

--- src/pages/summary.jsx
+++ src/pages/summary.jsx
@@ -19,17 +19,17 @@ import {
 } from "@material-ui/core";
 import io from "socket.io-client";
 import { withStyles } from "@material-ui/styles";
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect } from "react";
 import TopBar from "../components/topbar";
 import "../stylesheets/App.css";
 // import "../stylesheets/activeOrders.css";
 import { Redirect, Link } from "react-router-dom";
-import { mdiFormatLetterCase, mdiDotsVertical, mdiMagnify, mdiCloseThick  } from '@mdi/js';
+import { mdiFormatLetterCase, mdiDotsVertical, mdiMagnify } from '@mdi/js';
 import Icon from '@mdi/react';
 import { ResponsiveBar } from '@nivo/bar';
 import "../stylesheets/progressBar.css";
 import CountUp from "react-countup";
-import { borderColor, borderRadius, fontWeight, margin } from "@mui/system";
+import { borderRadius, fontWeight, margin } from "@mui/system";
 import DateFnsUtils from '@date-io/date-fns';
 import { DatePicker,KeyboardDatePicker, MuiPickersUtilsProvider  } from "@material-ui/pickers";
 import Moment from 'moment';
@@ -70,7 +70,6 @@ const [ topCards, setTopCards ] = useState({
   const handleChange = (event) => {
     setAge(event.target.value);
     setEnableAnimation(false)
-    clearTimeout(timer);
   };
   const [startDate, setStartDate] = useState(Moment(new Date()).format('DD-MMM-yyyy'));
   const [endDate, setEndDate] = useState(Moment(new Date()).format('DD-MMM-yyyy'));
@@ -81,7 +80,6 @@ const [ topCards, setTopCards ] = useState({
   const [loading, setLoading] = useState(true);
   const [enableAnimation, setEnableAnimation] = useState(true);
   const [msg, setMsg] = useState({});
-  const [msgH, setMsgH] = useState({});
   const [vendorTableDetails, setVendorTableDetails] = useState(
     {visible: true, 
       topCards: {
@@ -102,48 +100,27 @@ const [ topCards, setTopCards ] = useState({
     "INACTIVE_LINES":0,
     "ACTIVE_VENDORS":0,
     
-  }, topCardsH:{}, graphData: {}, lineGraph:[], tableData: [], tableDataH: [], inupt: {}}); // input will take the data for next screen API
-  const [factoryTableDetails, setFactoryTableDetails] = useState({visible: false, topCards: {}, topCardsH:{}, graphData: {}, lineGraph: [], tableData: [], tableDataH: [], input: {}});
+  }, topCardsH:{}, graphData: {}, lineGraph:[], tableData: [], inupt: {}}); // input will take the data for next screen API
+  const [factoryTableDetails, setFactoryTableDetails] = useState({visible: false, topCards: {}, topCardsH:{}, graphData: {}, lineGraph: [], tableData: [], input: {}});
   const [nextTableDetails, setNextTableDetails] = useState({currentTable : "", nextTable: "", details: {}}); 
   const [currentTable, setCurrentTable] = useState("vendor");
-  const [ searchWidthPh, setSearchWidthPh] = useState({width: "0px", ph: "Search brand or fgCode"});
+  const [ searchWidthPh, setSearchWidthPh] = useState({width: "0px", ph: ""});
   var timer;
-  const [sequenceType, setSequenceType] = useState({recent: "", orderQty: -1, pending: -1, pcsProduced: -1, okPcs: -1, rectifiedPcs: -1, pcsInAlter: -1, rejectedPcs: -1, rejectPer: -1, dhuPer: -1});
-  const [updateHistory, setUpdateHistory] = useState(1);
-  const [defectsHeatMap, setDefectsHeatMap] = useState([]);
-  const [fgCodeKPIdata, setFgCodeKPIdata] = useState([]);
- 
   const socketRef = props.data.socketRef;
-  const multiselectRef = React.useRef();
   useEffect(() => {
     if(props.data.loginState === 1)
     {
-      socketRef.current.on("fromServer", ( msg1 ) => {
-        console.log("message summary!!",msg1);
-        
-        let temp = msg1;
+      socketRef.current.on("fromServer", ( msg ) => {
+        // console.log("message summary!!",msg);
+        let temp = msg;
         temp.vendorGraphData.producedPieces.sort((a,b) => a["Produced Pieces"] - b["Produced Pieces"]).reverse();
         temp.vendorGraphData.okPieces.sort((a,b) => a["Ok Pieces"] - b["Ok Pieces"]).reverse();
         temp.vendorGraphData.alteredPieces.sort((a,b) => a["Altered Pieces"] - b["Altered Pieces"]).reverse();
         temp.vendorGraphData.rejectedPieces.sort((a,b) => a["Rejected Pieces"] - b["Rejected Pieces"]).reverse();
         temp.vendorGraphData.allPieces.sort((a,b) => a["All Pieces"] - b["All Pieces"]).reverse();
         temp.vendorGraphData.dhu.sort((a,b) => a["DHU"] - b["DHU"]).reverse();
-        setMsg((arg)=>{
-          setMsgH(JSON.parse(JSON.stringify({...arg})))
-          return(temp)})
+        setMsg(temp);
       })
-      socketRef.current.on("filterObjects", ( msg ) => {
-        // console.log("message summary!!",msg);
-        console.log("filter list is!!!!!!!!!!!!!!!!", msg)
-        let temp = msg;
-        // console.log("data is", data.result);
-        for(let i = 0; i< temp.length; i++)
-        {
-          temp[i].cat = temp[i].filterID;
-          temp[i].key = temp[i].filterValue;
-        }
-        setFgCodeList(temp);
-      });
       socketRef.current.on("connect", () => {
         // console.log("socket id summary!!!!!",socketRef.current.id); 
       });
@@ -173,11 +150,6 @@ const [ topCards, setTopCards ] = useState({
   }
   },[age])
 
-  useEffect (() =>{
-    if(sequenceType.recent !== "")
-    sequenceChange();
-  }, [sequenceType])
-
   useEffect(() =>{
     let temp = []
     if(msg.vendorLineGraph && msg.vendorLineGraph.length != 0)
@@ -186,13 +158,10 @@ const [ topCards, setTopCards ] = useState({
     }
     switch(currentTable)
     {
-      case "vendor": setVendorTableDetails({...vendorTableDetails, topCardsH: vendorTableDetails.topCards,  topCards: msg.topCards, graphData: msg.vendorGraphData, lineGraph: temp, tableDataH: msgH.vendorTableData ? msgH.vendorTableData : vendorTableDetails.tableData, tableData: msg.vendorTableData}); break;
-      case "factory": setFactoryTableDetails({...factoryTableDetails,  topCardsH: factoryTableDetails.topCards, topCards: msg.topCards, graphData: msg.vendorGraphData, lineGraph: temp, tableDataH: msgH.vendorTableData ? msgH.vendorTableData : vendorTableDetails.tableData, tableData: msg.vendorTableData}); break;
-      default: setVendorTableDetails({...vendorTableDetails, topCardsH: vendorTableDetails.topCards, topCards: msg.topCards, graphData: msg.vendorGraphData, lineGraph: temp, tableDataH: msgH.vendorTableData ? msgH.vendorTableData : vendorTableDetails.tableData, tableData: msg.vendorTableData}); break;
+      case "vendor": setVendorTableDetails({...vendorTableDetails, topCardsH: vendorTableDetails.topCards,  topCards: msg.topCards, graphData: msg.vendorGraphData, lineGraph: temp, tableData: msg.vendorTableData}); break;
+      case "factory": setFactoryTableDetails({...factoryTableDetails,  topCardsH: factoryTableDetails.topCards, topCards: msg.topCards, graphData: msg.vendorGraphData, lineGraph: temp, tableData: msg.vendorTableData}); break;
+      default: setVendorTableDetails({...vendorTableDetails, topCardsH: vendorTableDetails.topCards, topCards: msg.topCards, graphData: msg.vendorGraphData, lineGraph: temp, tableData: msg.vendorTableData}); break;
     }
-    setDefectsHeatMap(msg.defectsHeatMap);
-    setFgCodeKPIdata(msg.fgCodeKPIData);
-    sequenceChange();
     setLoading(false);
   },[msg])
 
@@ -210,71 +179,31 @@ const [ topCards, setTopCards ] = useState({
     }
   },[nextTableDetails.nextTable]);
 
-  // useEffect(() =>{
-  //   fetch(` https://zedqwsapi.bluekaktus.com/filters/getFiltersList`, {
-  //     method: "POST",
-  //     body: JSON.stringify({
-  //       "userID": 0,
-  //       "companyID": 0
-  //   })
-  //   })
-  //     .then((response) => response.json())
-  //     .then((data) => {
-  //   //         {
-  //   //   cat: 'Group 1',
-  //   //   key: 'Option 1'
-  //   // },
-  //       let temp = data.result;
-  //       // console.log("data is", data.result);
-  //       for(let i = 0; i< temp.length; i++)
-  //       {
-  //         temp[i].cat = temp[i].filterCode;
-  //         temp[i].key = temp[i].filterCode === "BRAND" ? "Brand Name: " + temp[i].filterDetails.BRAND_NAME :"Order No.: " + temp[i].filterDetails.ORDER_NO + "Style No.: " + temp[i].filterDetails.STYLE_NO
-  //       }
-  //       setFgCodeList(temp);
-  //     });
-  // }, [])
+  useEffect(() =>{
+    fetch(` https://zedqwsapi.bluekaktus.com/filters/getFiltersList`, {
+      method: "POST",
+      body: JSON.stringify({
+        "userID": 0,
+        "companyID": 0
+    })
+    })
+      .then((response) => response.json())
+      .then((data) => {
+    //         {
+    //   cat: 'Group 1',
+    //   key: 'Option 1'
+    // },
+        let temp = data.result;
+        // console.log("data is", data.result);
+        for(let i = 0; i< temp.length; i++)
+        {
+          temp[i].cat = temp[i].filterCode;
+          temp[i].key = temp[i].filterCode === "BRAND" ? "Brand Name: " + temp[i].filterDetails.BRAND_NAME :"Order No.: " + temp[i].filterDetails.ORDER_NO + "Style No.: " + temp[i].filterDetails.STYLE_NO
+        }
+        setFgCodeList(temp);
+      });
+  }, [])
 
-  const sequenceChange = () =>{
-    let key = "", message = JSON.parse(JSON.stringify(msg));
-
-    switch(sequenceType.recent){
-      case "orderQty": key = "orderQty"; break;
-      case "pending": key = "pendingPieces"; break;
-      case "pcsProduced": key = "producedPieces"; break;
-      case "okPcs": key = "okPieces"; break;
-      case "rectifiedPcs": key = "alteredPieces"; break;
-      case "pcsInAlter": key = "pcsInAlteration"; break;
-      case "rejectedPcs": key = "rejectedPieces"; break;
-      case "rejectPer": key = "rejPerc"; break;
-      case "dhuPer": key = "dhu"; break;
-    }
-    if(key !== "")
-    {
-      if(sequenceType[sequenceType.recent] === 1)
-      {
-        
-        if(key !== "rejPerc" && key !== "dhu")    
-        message.vendorTableData.sort((a,b) => {if(a["locationName"] !== "Total") {return a[key] - b[key]} else{return -10000}}).reverse(); // last value should not be less than -10000 or else total will be above it
-        else
-        message.vendorTableData.sort((a,b) => {if(a["locationName"] !== "Total") {return parseFloat(a[key].slice(0, a[key].length - 1)) - parseFloat(b[key].slice(0, b[key].length - 1))} else{return -10000}}).reverse(); // last value should not be less than -10000 or else total will be above it
-      }
-      switch(currentTable)
-    {
-      case "vendor": setVendorTableDetails({...vendorTableDetails, tableData: message.vendorTableData}); break;
-      case "factory": setFactoryTableDetails({...factoryTableDetails, tableData: message.vendorTableData}); break;
-      default: setVendorTableDetails({...vendorTableDetails, tableData: message.vendorTableData}); break;
-    }
-    }
-    if(sequenceType[sequenceType.recent] === 1)
-    {
-      setUpdateHistory(0);
-    }
-    else
-    {
-      setUpdateHistory(1);
-    }
-  }
 
 const handleTimeChange = (event) => {
   setSelectedDate(event.target.value);
@@ -284,24 +213,12 @@ const handleTimeChange = (event) => {
 };
 
 const handleFgCodeChange = (event) => {
-  setLoading(true);
-  let temp = event[0]; // Ideally temp should be an array and this line should throw error but code works fine with this and if temp is converted to and array, it gives error.
-  setSelectedFgCode(temp); //  remove when want multi
-  if(event[0].filterType === "")
-  {
-  setSearchWidthPh({width: "0px", ph: "Search brand or fgCode"})
-
-  }
-  else
-  {
-  setSearchWidthPh({...searchWidthPh, ph: event[0].filterType + ": "+ event[0].filterValue})
-  }
+  setSelectedFgCode(event);
   console.log("event!!!!!!!!!!!!1", event);
-  multiselectRef.current.resetSelectedValues(event);
-//  if(event.length > 0)
-  props.data.socketRef.current.emit("setFilters", [event[0]]);
-// else
- // props.data.socketRef.current.emit("setFilters", []);
+  if(event.length > 0)
+  props.data.socketRef.current.emit("setFgCodesFilter", [event[0]]);
+  else
+  props.data.socketRef.current.emit("setFgCodesFilter", []);
 }
 
 if (props.data.loginState !== 1) {
@@ -342,7 +259,7 @@ if (props.data.loginState !== 1) {
                       label="Start Date"
                       format="dd-MMM-yyyy"
                       value={startDate}
-                      onChange={date => {console.log("date is !!!!!!!!!!!!", Moment(date).format('DD-MMM-yyyy hh:mm:ss'));setStartDate(Moment(date).format('DD-MMM-yyyy'))}}
+                      onChange={date => setStartDate(Moment(date).format('DD-MMM-yyyy'))}
                       InputAdornmentProps={{ position: "start" }}
                       maxDate={endDate}
                     />
@@ -378,7 +295,7 @@ if (props.data.loginState !== 1) {
               label="End Date"
               format="dd-MMM-yyyy"
               value={endDate}
-              onChange={date => { console.log("date is !!!!!!!!!!!!", Moment(date).format('DD-MMM-yyyy'));setEndDate(Moment(date).format('DD-MMM-yyyy'));}}
+              onChange={date => setEndDate(Moment(date).format('DD-MMM-yyyy'))}
               InputAdornmentProps={{ position: "start" }}
               minDate={startDate}
             />
@@ -420,9 +337,9 @@ if (props.data.loginState !== 1) {
     verticalAlign: "middle",
     outline: "none",}}onChange={event => setSearchText(event.target.value)} /> */}
     <div style ={{display: 'flex', maxWidth: "600px", marginLeft: 20, marginRight: 20 }}                         
-    onMouseEnter={() => setSearchWidthPh({ ... searchWidthPh, width: "550px"})}
+    onMouseEnter={() => setSearchWidthPh({width: "550px", ph: "Search Brand or FgCode"})}
     onMouseLeave={() => {if(selectedFgCode.length === 0)
-                          setSearchWidthPh({...searchWidthPh, width: "0px"})
+                          setSearchWidthPh({width: "0px", ph: ""})
                         }}>
     <Icon path={mdiMagnify}
                         size={2}
@@ -433,15 +350,11 @@ if (props.data.loginState !== 1) {
       displayValue="key"
       id="css_custom"
       //onKeyPressFn={function noRefCheck(){}}
-    //  onRemove={(e) => handleFgCodeChange(e)}
+      onRemove={(e) => handleFgCodeChange(e)}
       // onSearch={(e) => console.log ("searched", e)}
-      onSelect={(e) => setTimeout(() => handleFgCodeChange(e), 300)} // state take a little time to update event hence added some delay
+      onSelect={(e) => handleFgCodeChange(e)}
       options={fgCodeList}
-    //  singleSelect={true}
-    //selectionLimit={1}
-    selectedValues={selectedFgCode} // remove when want multi
-      placeholder={searchWidthPh.ph} 
-      ref={multiselectRef}
+      placeholder={searchWidthPh.ph}
       style={{
         chips: {
           background: 'blue'
@@ -460,32 +373,27 @@ if (props.data.loginState !== 1) {
           width: searchWidthPh.width,
       }
       }}
-      />
-      {searchWidthPh.width !== "0px" ? 
-        <Icon path={mdiCloseThick} size={2} color="blue" onClick={() =>{handleFgCodeChange([{ filterID: 0, filterType: "", filterValue: ""}])}}  /> : null }
+/>
 </div>
     <div><Button   onClick={() => {
       // console.log("socket.current", socketRef.current);
     //  socketRef.current.disconnect();
       props.data.setLoginState(-1);
     //  props.data.setSocketID("");
-  }}>SignOut</Button> 
+  }}>SignOut</Button>
   </div>
         </Grid>
         {vendorTableDetails.visible ? <VendorGraph age={age} handleChange={handleChange} topCardsH={vendorTableDetails.topCardsH} topCards={vendorTableDetails.topCards} graphData={vendorTableDetails.graphData} lineGraph={vendorTableDetails.lineGraph} />:null}
         {factoryTableDetails.visible ? <VendorGraph age={age} handleChange={handleChange} topCardsH={factoryTableDetails.topCardsH} topCards={factoryTableDetails.topCards} graphData={factoryTableDetails.graphData} lineGraph={factoryTableDetails.lineGraph}/>:null}
       </section>
-      {fgCodeKPIdata && fgCodeKPIdata.length >0 ?
       <section className="two">
-        <FgCards fgCodeKPIdata={fgCodeKPIdata} defectsHeatMap={defectsHeatMap}/>
+        <FgCards />
       </section>
-      : null}
       <section className="three">
       <div className="wrapper">
-
               <div className={classes.tableO}>
-              {vendorTableDetails.visible ? <VendorTable data={vendorTableDetails.tableData} nextTableFunc={setNextTableDetails} tableDataH={vendorTableDetails.tableDataH} setSequenceType={setSequenceType} sequenceType={sequenceType} setUpdateHistory={setUpdateHistory} updateHistory={updateHistory} searchWidthPh={searchWidthPh}/> : null}
-              {factoryTableDetails.visible ? <FactoryTable data={factoryTableDetails.tableData} nextTableFunc={setNextTableDetails}  tableDataH={factoryTableDetails.tableDataH} setSequenceType={setSequenceType} sequenceType={sequenceType} setUpdateHistory={setUpdateHistory} updateHistory={updateHistory} searchWidthPh={searchWidthPh}/> : null}
+              {vendorTableDetails.visible ? <VendorTable data={vendorTableDetails.tableData} nextTableFunc={setNextTableDetails} /> : null}
+              {factoryTableDetails.visible ? <FactoryTable data={factoryTableDetails.tableData} nextTableFunc={setNextTableDetails} /> : null}
               </div>
             </div>
       </section>
