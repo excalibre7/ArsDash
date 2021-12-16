aa7fcd51e0f96b7e1833f09b9babe597a65955ed
Sambhasher <sambhavj9@gmail.com>
2021-12-16 09:47:24 +0530

    Loader

--- src/pages/summary.jsx
+++ src/pages/summary.jsx
@@ -352,26 +352,14 @@ if (props.data.loginState !== 1) {
                     />
     </MuiPickersUtilsProvider>
 }
-{/* {loading?
-<div style={{width:300,height:"7vh",justifyContent:"center",alignContent:"center",alignItems:"center"}}>
-      <div class="loader">
-      <svg viewBox="0 0 80 80">
-          <circle id="test" cx="40" cy="40" r="32"></circle>
-      </svg>
-    </div>
-
-    <div class="loader triangle">
-      <svg viewBox="0 0 86 80">
-          <polygon points="43 8 79 72 7 72"></polygon>
-      </svg>
-    </div>
-
-    <div class="loader">
-      <svg viewBox="0 0 80 80">
-          <rect x="8" y="8" width="64" height="64"></rect>
-      </svg>
-    </div>
-</div>:null} */}
+<div class="containerx">
+  <p class="icon">HOVER ME</p>
+  <div class="popup">
+    <p>Popup which contains some crazy cool information!</p>
+  </div>
+</div>
+{loading?
+  <div class="continuous-4"></div>:null}
 
 {selectedDate === "custom" &&
     <MuiPickersUtilsProvider utils={DateFnsUtils}>
